622297f439019b44dc5b8a6143eefe2389a25f51
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2016-04-08 14:47:09 +0200

    Production build

--- gulpfile.js
+++ gulpfile.js
@@ -119,8 +119,7 @@ gulp.task('js:vendor', function() {
   return gulp.src([
     './node_modules/prismjs/prism.js',
     './node_modules/prismjs/components/prism-bash.js',
-    './node_modules/leaflet/dist/leaflet.js',
-    './node_modules/components-webfontloader/webfont.js'
+    './node_modules/leaflet/dist/leaflet.js'
   ])
   .pipe(plumber(errHandler))
   .pipe(concat('dist-vendor.min.js'))
